cdad96b2f8066af757a97f4b4075730ca74e4a3f
Jerico Dcosta <68363466+Jerico007@users.noreply.github.com>
2023-07-18 16:59:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
     //Write your code here
-
+	 let elements = document.getElementsByTagName("strong");
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].style.color = "green";
+  }
+	
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	 let elements = document.getElementsByTagName("strong");
+	  for (let i = 0; i < elements.length; i++) {
+    elements[i].style.color = "black";
+	  }
     
 }
